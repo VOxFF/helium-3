67918a7480b01f9e02a73ab260f2bc33ae1ec349
Volodymyr Dubovyi <volodymyr.dubovyi@gmail.com>
2025-06-25 22:07:52 -0700

    wip

--- simulation/interfaces/ILog.h
+++ simulation/interfaces/ILog.h
@@ -2,11 +2,26 @@
 
 #include "common/Event.h"
 
+#include <map>
+
 namespace Helium3 {
 
 class ILog {
 public:
     virtual ~ILog() = default;
+
+    struct StateSummary {
+        size_t occurrences = 0;
+        Duration totalDuration{};
+        std::string message;
+    };
+    struct MachineSummary {
+        std::string machineId;
+        std::map<int, StateSummary> complette;
+        std::map<int, StateSummary> unfinshed;
+    };
+
+
 
     virtual void add(const Event& e) = 0;
     
